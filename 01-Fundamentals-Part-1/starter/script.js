fc0db163a3e0aef7f9f7b0782429a192f13dfeec
Raelyn Brown <msraerae2000@gmail.com>
2021-12-06 03:48:00 -0600

    commit to master

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Jonas')
+console.log(23)
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
