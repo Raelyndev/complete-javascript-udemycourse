786cb8d7ac32f8c2f043f36234c30aa352df6589
Raelyn Brown <msraerae2000@gmail.com>
2021-12-08 00:31:43 -0600

    commit to master

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,5 +1,7 @@
+/* 
 let country = "United States";
 let continent = "North America";
 let population = '200,000,000';
 
 console.log(country, continent, population);
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,15 +1,62 @@
+/* 
 let js = 'amazing';
 console.log(40+8+23-10);
 
-console.log('Jonas')
-console.log(23)
+console.log('Jonas');
+console.log(23);
+
 let firstName = "Matilda";
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+
 let myFirstJob = 'Coder';
 let myCurrentJob = 'Teacher';
 
 console.log(myFirstJob);
+
+
+
+// Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof'Jonas');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+ // Let, const and Var
+
+ /* 
+let age = 30;
+age = 31;
+
+const birthYear = 1991; 
+birthYear = 1990;
+
+const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Brown';
+console.log(lastName);
+*/
+
