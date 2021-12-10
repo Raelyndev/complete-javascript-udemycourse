84055fe7198fe3991966b5e7e5160e1fafaace5e
Raelyn Brown <msraerae2000@gmail.com>
2021-12-10 06:14:52 -0600

    complete js assignments

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,7 +1,32 @@
-/* 
-let country = "United States";
-let continent = "North America";
+
+
+// Values and Variables
+
+const country = 'United States';
+const continent = "North America";
 let population = '200,000,000';
 
 console.log(country, continent, population);
-*/
+
+
+// Data Types
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof  country);
+console.log(typeof  language);
+
+
+// let, const, var
+ 
+language = 'English';
+console.log(isIsland, population, country, continent, language);
+
+// Basic Operations
+
+
+
+     
