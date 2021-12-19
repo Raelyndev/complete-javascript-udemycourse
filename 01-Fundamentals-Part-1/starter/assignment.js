b4f0db683db6a3cd84156dfcec22d50cc0d4a654
Raelyn Brown <msraerae2000@gmail.com>
2021-12-18 22:29:27 -0600

    commit

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -2,7 +2,6 @@
 
 // Values and Variables
 
-/*
 const country = 'United States';
 const continent = "North America";
 let population = 13;
@@ -55,4 +54,3 @@ console.log('19' - '13' + '17'); // 617
 console.log('19' - '13' + 17); // 23
 console.log('123' < 57); // boolean false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
-*/
