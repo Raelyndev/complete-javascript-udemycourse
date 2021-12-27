74e23e464150ccfe81d4179d2b8e791a71e40c6c
Raelyn Brown <msraerae2000@gmail.com>
2021-12-26 23:49:14 -0600

    funadmentals 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+// activating strict mode
+
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+
+const interface = 'Audio';
