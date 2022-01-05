305d3f50070ad32a2eec23c99cb8e4c7ef13fb52
Raelyn Brown <msraerae2000@gmail.com>
2022-01-04 21:30:26 -0600

    reviewing functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -69,5 +69,45 @@ const yearsUntilRetire = (birthYear, firstName) => {
 }
 console.log(yearsUntilRetire(1991, 'Jonas'));
 console.log(yearsUntilRetire(1980, 'bob')); 
+
+
+
+// Functions Calling Other Functions
+
+function cutFruitPieces(fruit) {
+     return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+     const applePieces = cutFruitPieces(apples);
+     const orangePieces = cutFruitPieces(oranges);
+     const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} Pieces of oranges.`
+     return juice;
+}
+
+console.log(fruitProcessor(2,3));
 */
+
+
+// Function Review
+
+const calcAge = function(birthYear) {
+     return 2037 - birthYear;
+}
+
+const yearsUntilRetire = function (birthYear, firstName) {
+     const age = calcAge(birthYear);
+     const retirement = 65 - age;
+
+     if (retirement > 0) {
+          console.log(`${firstName} retires in ${retirement} years`)
+          return retirement;
+     } else {
+          console.log(`${firstName} has already retired`);
+          return -1;
+     }
+}
+
+console.log(yearsUntilRetire(1991, 'Jonas'));
+console.log(yearsUntilRetire(1970, 'Mike'));
 
